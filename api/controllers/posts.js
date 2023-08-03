2ab96f4b6518b02ca1b417297c6d1fd8dea64fb7
Liam_Coles <lcoles1988@gmail.com>
2023-08-03 12:04:07 +0100

    added logic and tests to handle when no id is passed in or property that does not exist on posts collection is passed in

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -23,16 +23,21 @@ const PostsController = {
   },
   UpdateById: (req, res) => {
     const postId = req.body.id;
-    if (req.body.hasOwnProperty('comments')){
-        const comments = req.body.comments;p
-        console.log(comments);
+    if (req.body.hasOwnProperty('id') === false){
+      res.status(400).json({ message: 'id not present'});
+    } else {
+      if (req.body.hasOwnProperty('comments')){
+        const comments = req.body.comments;
         Post.updateOne({_id:postId}, {$push:{comments: comments}}, (err) => {
-            if (err) {
-              throw err;
-            }
-        const token = TokenGenerator.jsonwebtoken(req.user_id)
-        res.status(201).json({ message: 'OK', token: token });
+          if (err) {
+            throw err;
+          }
+          const token = TokenGenerator.jsonwebtoken(req.user_id)
+          res.status(201).json({ message: 'OK', token: token });
         })
+      } else {
+        res.status(400).json({ message: 'property not found'});
+      }
     }
   }
 };
