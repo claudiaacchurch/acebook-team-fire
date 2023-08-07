a85a910b76bf064412edcfc8a3ca0bcf2a21f8a7
Bradley <brad.holmes05@icloud.com>
2023-08-07 12:13:20 +0100

    Create @me route

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -6,31 +6,50 @@ const UsersController = {
     const user = new User(req.body);
     user.save((err) => {
       if (err) {
-        res.status(400).json({message: 'Bad request'})
+        res.status(400).json({ message: "Bad request" });
       } else {
-        res.status(201).json({ message: 'OK' });
+        res.status(201).json({ message: "OK" });
       }
     });
   },
   GetUserByID: async (req, res) => {
-    const userID = req.params.id
-    const authenticated_user_id = req.user_id
+    const userID = req.params.id;
+    const authenticated_user_id = req.user_id;
 
     try {
-      const user = await User.findById(userID)
-    if (! user) {
-      res.status(404).json({message: 'User not found'})
-    } else if (userID !== authenticated_user_id) {
-      res.status(401).json({message: "Unauthorised User"})
-    } else {
-      res.status(200).json({username: user.username, email: user.email})
-    }
+      const user = await User.findById(userID);
+      if (!user) {
+        res.status(404).json({ message: "User not found" });
+      } else if (userID !== authenticated_user_id) {
+        res.status(401).json({ message: "Unauthorised User" });
+      } else {
+        res.status(200).json({ username: user.username, email: user.email });
+      }
     } catch (error) {
-      console.error(error)
-      res.status(500).json({message: 'server error'})
+      console.error(error);
+      res.status(500).json({ message: "server error" });
     }
-    
-  }
+  },
+
+  Me: (req, res) => {
+    const token = req.headers.authorization.replace("Bearer ", "");
+    const { user_id } = TokenGenerator.verify(token);
+
+    User.findById(user_id, (err, user) => {
+      if (err) res.status(500).end();
+
+      const { email, username, profilePic } = user;
+
+      console.log(user);
+      console.log(email, username, profilePic);
+
+      res.status(200).json({
+        email,
+        username,
+        profilePic,
+      });
+    });
+  },
 };
 
 module.exports = UsersController;
