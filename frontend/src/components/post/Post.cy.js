37369a0fb0da2f2695cbe7f1ecf70cb61d0fa166
Liam_Coles <lcoles1988@gmail.com>
2023-08-08 15:21:30 +0100

    added tests for createcomment component

--- frontend/src/components/post/Post.cy.js
+++ frontend/src/components/post/Post.cy.js
@@ -5,4 +5,33 @@ describe("Post", () => {
     cy.mount(<Post post={{_id: 1, message: "Hello, world"}} />);
     cy.get('[data-cy="post"]').should('contain.text', "Hello, world")
   })
+  it('renders a commentForm', () => {
+    cy.mount(<Post post={{_id: 1, message: "Hello, world"}} />);
+    cy.get('[data-cy="submit-comment"]').should('exist');
+    cy.get('[data-cy="comment-text"]').should('exist');
+  })
+})
+
+describe("submitComment" ,() => {
+  it('fetches patch /posts with correct, datetime, authorid, message and postID', () => {
+    cy.intercept('GET', '/@ME', {
+      statusCode: 200,
+      body: {username: "Barry123"}
+    });
+    cy.intercept('POST', '/posts/15', {
+      statusCode: 200
+    }).as('post-posts');
+    let mockDate = new Date(2023, 5, 27, 9, 56, 16);
+    cy.clock(mockDate)
+    cy.mount(<Post post={{_id: 15, message: "Hello, world"}} />);
+    cy.get('[data-cy="comment-text"]').type("I am a comment");
+    cy.get('[data-cy="submit-comment"]').click();
+    cy.wait("@post-posts").then((interception) =>{ 
+      const requestbody = JSON.parse(interception.request.body);
+      expect(requestbody.comments.authorName).to.eq('Barry123');
+      expect(requestbody.comments.text).to.eq("I am a comment");
+      expect(requestbody.comments.commentDate).to.eq("2023-06-27T08:56:16.000Z")
+    })
+
+  })
 })
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CommentForm from '../commentForm/CommentForm';
 
 const Post = ({post}) => {
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
+
+  const submitComment = async (commentText) => {
+    let response = await fetch('/@ME', {headers: {'Authorization': `Bearer ${token}`}});
+    let data = await response.json();
+    window.localStorage.setItem("token", data.token);
+    setToken(window.localStorage.getItem("token"));
+    const comment = {text: commentText, authorName: data.username, commentDate: new Date()};
+    response = await fetch(`/posts/${post._id}`, {method: 'POST', headers: {'Authorization': `Bearer ${token}`}, body: JSON.stringify({'comments':comment})});
+  }
+
+
   return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
+    <div>
+      <article data-cy="post" key={ post._id }>{ post.message }</article>
+      <CommentForm submitComment = {submitComment}/>
+    </div>
   )
 }
 
