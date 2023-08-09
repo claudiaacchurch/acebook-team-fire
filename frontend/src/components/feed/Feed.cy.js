d62765209e8767f10475dac5d688a8d8f026386a
Liam_Coles <lcoles1988@gmail.com>
2023-08-09 14:34:22 +0100

    new attempt to tix tests

--- frontend/src/components/feed/Feed.cy.js
+++ frontend/src/components/feed/Feed.cy.js
@@ -122,16 +122,12 @@ describe("Feed", () => {
     cy.mount(<Feed navigate={navigate} />);
     cy.wait("@getPosts").then(()=>{
       cy.get('[class="like-btn-1"]').click();
-    });
-    
-    cy.wait("@patchPosts").then(() => {
       cy.get('[class="like-btn-1"]').click();
       cy.get('[class="like-btn-1"]').click();
     }).then(() => {
       cy.get('[data-cy="post"]').eq(0).should("contain.text", "Hello, world 5Like");
       cy.get('[data-cy="post"]').eq(-1).should("contain.text", "Hello again, world 2Like");
     });
-    
     
   });
 });
