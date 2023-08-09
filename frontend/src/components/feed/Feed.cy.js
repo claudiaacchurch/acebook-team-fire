2f38dd66cebab4a0264fadb83fbf1c44b7629bbb
Liam_Coles <lcoles1988@gmail.com>
2023-08-09 14:29:13 +0100

    nth attempt to fix tests

--- frontend/src/components/feed/Feed.cy.js
+++ frontend/src/components/feed/Feed.cy.js
@@ -120,16 +120,18 @@ describe("Feed", () => {
     }).as("patchPosts");
 
     cy.mount(<Feed navigate={navigate} />);
-    cy.wait("@getPosts");
-    cy.get('[class="like-btn-1"]').click();
-    cy.wait("@patchPosts");
-    cy.get('[class="like-btn-1"]').click();
-    cy.wait("@patchPosts");
-    cy.get('[class="like-btn-1"]').click();
-    cy.wait(1000);
+    cy.wait("@getPosts").then(()=>{
+      cy.get('[class="like-btn-1"]').click();
+    });
+    
     cy.wait("@patchPosts").then(() => {
+      cy.get('[class="like-btn-1"]').click();
+      cy.get('[class="like-btn-1"]').click();
+    }).then(() => {
       cy.get('[data-cy="post"]').eq(0).should("contain.text", "Hello, world 5Like");
       cy.get('[data-cy="post"]').eq(-1).should("contain.text", "Hello again, world 2Like");
     });
+    
+    
   });
 });
