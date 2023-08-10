014f3248522a95dbab4ebfbb463c43cd5613a921
Oliver Loftus <oliverloftus1@gmail.com>
2023-08-10 11:41:24 +0100

    refactor Navbar and sweep up

--- frontend/src/components/Profile/profile.js
+++ frontend/src/components/Profile/profile.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Navbar from '../navbar/Navbar';
 
 
 const Profile = ({ navigate }) => {
 
     return (
     <div>
-        <Navbar navigate={navigate} />
-        
     Username: {}
     Email: {}
     </div>
