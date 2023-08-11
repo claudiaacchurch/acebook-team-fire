cd76c2ac49a4fb30c30c80cba25e92cf6194f453
Daria Zvarich <dorimoriza@gmail.com>
2023-08-11 04:10:58 -0700

    Solve a test in post

--- frontend/src/components/post/Post.cy.js
+++ frontend/src/components/post/Post.cy.js
@@ -12,8 +12,10 @@ describe("Post", () => {
   })
   
   it('renders a post with a message and likes with have.text', () => {
-    cy.mount(<Post post={{_id: 1, message: "Hello, world", likes:5, comments: []}} />);
-    cy.get('[data-cy="post"]').should('have.text', "Hello, world 5Like")
+    const comment1 = {_id: 1, text: "Hello, world",  username: 'the best author name', commentDate: "2023-01-01"}
+    cy.mount(<Post post={{_id: 1, message: "Hello, world", likes:5, comments: [comment1]}} />);
+    cy.get('[data-cy="post"]').should('have.text', "Hello, world")
+    cy.get('[data-cy="likes"]').should('have.text', "5")
   })
 
   it('checks Like button exists', () => {
@@ -25,9 +27,10 @@ describe("Post", () => {
   it('renders a post with a multiple comments', () => {
     const comment1 = {_id: 1, text: "Hello, world",  username: 'the best author name', commentDate: "2023-01-01"};
     const comment2 = {_id: 2, text: "Bye, world",  username: '123', commentDate: "2023-02-02"};
-    cy.mount(<Post post={{_id: 1, message: "Hello, world", likes:5, comments: [comment1, comment2]}} />);
+    cy.mount(<Post post={{_id: 1, message: "Hello, world", likes:5, comments: [comment1, comment2], image: "something"}}/>)
     cy.get('[data-cy="comment"]').eq(0).should('contain.text', "Hello, worldBy: the best author nameDate: 2023-01-01")
     cy.get('[data-cy="comment"]').eq(-1).should('contain.text', "Bye, worldBy: 123Date: 2023-02-02")
+
   })
 })
 
